d5e0ba0494ca1e786b27b77be14f34dafbe9b7b6
Andre Tavara <andretj_92@live.com>
2026-06-10 21:02:22 -0500

    Agrega la clase del miercoles 10 de junio

--- handlers/viewHandlers.js
+++ handlers/viewHandlers.js
@@ -1,5 +1,8 @@
 import { sendHtml } from "../utils/response.js";
 import { getLayout } from "../utils/html.js";
+import { parseUrlEncoded } from "../utils/bodyParser.js";
+import { readFile, writeFile } from "node:fs/promises";
+import path from "node:path";
 
 export async function getHome(_req, res) {
   const content = `
@@ -48,4 +51,58 @@ export async function getNewContact(req, res) {
 
   const html = getLayout("Nuevo Contacto", content);
   sendHtml(res, html);
+}
+
+const DATA_DIR = path.join(import.meta.dirname, "../data");
+const MESSAGES_FILE = path.join(DATA_DIR, "messages.json");
+
+export async function postContact(req, res) {
+  // sanitizacion o parseado del body
+  const body = await parseUrlEncoded(req);
+  console.log(body);
+
+  // validacion de los datos
+  const { name, email, message } = body;
+
+  if (!name || !email || !message) {
+    res.writeHead(400); // 400 Bad Request
+    return res.end();
+  }
+
+  // persistencia de datos
+  let messages = [];
+
+  try {
+    const fileContent = await readFile(MESSAGES_FILE, "utf-8");
+    messages = JSON.parse(fileContent);
+  } catch (error) {
+    if (error.code === "ENOENT") {
+      console.log("TEST");
+      await writeFile(MESSAGES_FILE, messages);
+    } else {
+      // Cualquier otro error se trata como un error de servidor.
+      const status = 500; // Internal Server Error
+      res.writeHead(status);
+      return res.end();
+    }
+  }
+
+  const newMessage = {
+    name,
+    email,
+    message,
+    timestamp: new Date().toISOString(),
+  };
+  messages.push(newMessage);
+  await writeFile(MESSAGES_FILE, JSON.stringify(messages, null, 2));
+
+  // Devolvemos una confirmación.
+  const content = `
+      <h1>Mensaje Recibido</h1>
+      <p>Gracias <strong>${name}</strong> (${email}). Hemos recibido tu mensaje.</p>
+      <a href="/">Volver al inicio</a>
+      `;
+
+  const html = getLayout("Confirmación", content);
+  return sendHtml(res, html);
 }
--- router.js
+++ router.js
@@ -1,7 +1,10 @@
 import { getHealth, getTime } from "./handlers/apiHandlers.js";
-import { getHome, getNewContact } from "./handlers/viewHandlers.js";
+import {
+  getHome,
+  getNewContact,
+  postContact,
+} from "./handlers/viewHandlers.js";
 import { staticHandler } from "./handlers/staticHandler.js";
-import { parseUrlEncoded } from "./utils/bodyParser.js";
 
 export async function router(req, res) {
   const pathname = req.url;
@@ -23,22 +26,7 @@ export async function router(req, res) {
   }
 
   if (pathname === "/contact" && method === "POST") {
-    // sanitizacion o parseado del body
-    const body = await parseUrlEncoded(req);
-    console.log(body);
-
-    // validacion de los datos
-    const { name, email, message } = body;
-
-    if (!name || !email || !message) {
-      res.writeHead(400); // 400 Bad Request
-      return res.end();
-    }
-
-    // return await saveNewContact(req, res);
-
-    res.writeHead(200);
-    return res.end("Mensaje recibido!");
+    return await postContact(req, res);
   }
 
   if (pathname === "/") {
